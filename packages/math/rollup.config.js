fe0b2189963d99209e7892cb2d32f53525f5a653
mac <mac@macdeMacBook-Air.local>
2025-04-29 15:35:10 +0800

    feat: 升级 math 库版本

--- packages/math/rollup.config.js
+++ packages/math/rollup.config.js
@@ -1,39 +1,25 @@
-import { getBanner, getPlugins } from '../../scripts/rollup-config-helper.js';
-import { readFileSync } from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { getBanner, getPlugins } from '../../scripts/rollup-config-helper';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const pkg = JSON.parse(readFileSync(resolve(__dirname, './package.json'), 'utf8'));
+const pkg = require('./package.json');
 const banner = getBanner(pkg);
 const plugins = getPlugins(pkg);
 
 export default () => {
-  return [{
-    input: 'src/index.ts',
-    output: [{
-      file: pkg.module,
-      format: 'es',
-      banner,
-      sourcemap: true,
-    }, {
-      file: pkg.main,
-      format: 'cjs',
-      banner,
-      sourcemap: true,
-    }],
-    plugins,
-  }, {
-    input: 'src/index.ts',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'srm.math',
-      banner,
-      sourcemap: true,
+  return [
+    {
+      input: 'src/index.ts',
+      output: [{
+        file: pkg.module,
+        format: 'es',
+        banner,
+        sourcemap: true,
+      }, {
+        file: pkg.main,
+        format: 'cjs',
+        banner,
+        sourcemap: true,
+      }],
+      plugins,
     },
-    plugins: getPlugins(pkg, { min: true }),
-  }];
+  ];
 };
